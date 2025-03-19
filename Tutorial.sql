47c072d1abb72f5e55337432a54ee592f4e9dfd7
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-03-18 21:52:51 -0300

    Add files via upload

---
+++ Tutorial.sql
@@ -0,0 +1,25 @@
+/*
+1) Iniciar el servidor MySQL:
+
+-En el Xampp se ingresa y se activa el MySQL y se accede a la Shell del mismo, NO se accede a Apache
+
+-En la Shell se escribe "Mysql -u root" Para ingresar a MariaDB como servidor de MySQL
+
+Base de Datos a usar:
+ https://github.com/santiagotrini/practica-1-queries
+
+ 2) Entrar a la Base de Datos:
+
+ -Ingresar a la Shell una vez activado MySQL (no ingresar a MariaDB aun)
+
+ -escribir "git clone url" siendo url la url de github mencionada ( git clone https://github.com/santiagotrini/practica-1-queries)
+
+ -Escribir "cd practica-1-queries"
+
+ -Escribir "mysql -u root < db_schema.sql"
+
+ -Escribir " mysql -u root < db_data.sql"
+
+ -Escribir "mysql -u root practica_1"
+
+*/
