9b26770b19d897b69a530e6201882cfc202a9808
Linkinshura <166653647+Linkinshura@users.noreply.github.com>
2025-05-20 22:13:45 -0300

    Create db-schema.sql

---
+++ BDAlumnos/db-schema.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE OliverSykes;
+
+USE OliverSykes;
+
+CREATE TABLE alumnos (
+    DNI INT UNSIGNED PRIMARY KEY,
+    nombres VARCHAR(255) NOT NULL,
+    apellidos VARCHAR(255) NOT NULL,
+    fecha_nac DATE NOT NULL,
+    curso INT UNSIGNED,
+    division INT UNSIGNED,
+    especialidad VARCHAR(50)  
+);
+
+
+CREATE TABLE telefonos (
+    id INT UNSIGNED PRIMARY KEY,
+    parentesco VARCHAR(50),
+    tel VARCHAR(15) NOT NULL,  
+    alumno INT UNSIGNED,
+    FOREIGN KEY (alumno) REFERENCES alumnos(DNI)
+);
+
+
+
+
+CREATE TABLE materias (
+    id INT UNSIGNED PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    horas INT UNSIGNED NOT NULL,
+    anio INT UNSIGNED NOT NULL,
+    especialidad VARCHAR(50) 
+);
+
+
+
+
+
+CREATE TABLE notas (
+    id INT UNSIGNED PRIMARY KEY,
+    tipo_calificacion VARCHAR(20) NOT NULL,
+    nota INT UNSIGNED NOT NULL,
+    fecha_carga DATE NOT NULL,
+    alumno INT UNSIGNED,
+    materia INT UNSIGNED,   
+    FOREIGN KEY (alumno) REFERENCES alumnos(DNI), 
+    FOREIGN KEY (materia) REFERENCES materias(id) 
+);
